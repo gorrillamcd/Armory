de8551383f023db950bb2fb7bca0fece261c286b
Tim Kaemmerer <tech@ccbcmexico.com>
2012-02-28 10:33:15 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Armory::Application.config.secret_token = 'cd4c7b365ba9f1b362aad8fdb4d3af3e3f5aefad2bbd641808fcc17baf9bd9d50f525448a40ea54b8d62cc5cb4cd743ac577103134941c89f559adf27475a946'
