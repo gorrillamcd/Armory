033626dbf4521985279ef10760b2e15f1d18e294
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2012-08-02 19:15:55 -0700

    Added view for Dashboard show action

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,14 @@
 class DashboardController < ApplicationController
 
+skip_authorization_check # TODO: Add authorization to this controller
+
 	def show
 		case current_user.role
 
 			when "admin"
-				@users = User.all
+				@recent_users = User.limit(5).order("created_at DESC").group_by(&:role)
 			when "staff"
-				return true
+				
 			when "teacher"
 				return true
 			when "student"
