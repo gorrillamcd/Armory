b64800fb2df8e9b2e650fd2b621ccbdb389563e8
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2012-09-18 22:06:35 -0700

    Fixed some browser inconsistencies on the homepage, franchise font still needs fixing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,20 +3,25 @@ class DashboardController < ApplicationController
 skip_authorization_check # TODO: Add authorization to this controller
 
 	def show
-		case current_user.role
+		if user_signed_in?
+			case current_user.role
 
-			when "admin" 		# TODO: Expand admin dashboard to provide all needed info
-				@recent_users = User.limit(5).order("created_at DESC").group_by(&:role)
-			when "staff"
-				return true 	# TODO: Add Staff Dashboard
-			when "teacher"
-				return true 	# TODO: Add Teacher Dashboard
-			when "student" 	# TODO: Expand Student dashboard to provide all needed info
-				@subscriptions = Subscription.where(:user_id => current_user.id).group_by(&:state)
-				@courses = current_user.courses.select('courses.*, subscriptions.state').group_by(&:state)
-			else
-				return false
-				
+				when "admin" 		# TODO: Expand admin dashboard to provide all needed info
+					@recent_users = User.limit(5).order("created_at DESC").group_by(&:role)
+				when "staff"
+					return true 	# TODO: Add Staff Dashboard
+				when "teacher"
+					return true 	# TODO: Add Teacher Dashboard
+				when "student" 	# TODO: Expand Student dashboard to provide all needed info
+					@subscriptions = Subscription.where(:user_id => current_user.id).group_by(&:state)
+					@courses = current_user.courses.select('courses.*, subscriptions.state').group_by(&:state)
+				else
+					return false
+					
+			end
+		else
+			redirect_to :root
+			flash[:error] = "You must be logged in first."
 		end
 	end
 
