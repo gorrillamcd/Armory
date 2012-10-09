88755091cce6133b99d590801fbb7e4230d119f3
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2012-10-09 12:20:14 -0700

    Refactored welcome_text helper to use join instead of concatenation

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -22,9 +22,10 @@ module LayoutHelper
 
   def welcome_text
     if user_signed_in?
-      @welcome_text = raw(t('application.welcome')+" "+current_user.full_name+", "+content_tag(:small, t('application.greeting')))
+      #@welcome_text = raw(t('application.welcome')+" "+current_user.full_name+", "+content_tag(:small, t('application.greeting')))
+      @welcome_text = raw([t('application.welcome'),current_user.full_name,content_tag(:small, t('application.greeting'))].join(" "))
     else
-      @welcome_text = t('application.welcome_to')+" "+t('application.name')
+      @welcome_text = [t('application.welcome_to'),t('application.name')].join(" ")
     end
   end
 
