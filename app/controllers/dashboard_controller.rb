c2086dc25a5f4fa8e1d10d603a17cb7b4ca36787
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2012-11-14 18:50:27 -0800

    Fixed links on student dashboard view and grouping on @courses query

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,9 +14,8 @@ skip_authorization_check # TODO: Add authorization to this controller
 				when "teacher"
 					return true 	# TODO: Add Teacher Dashboard
 				when "student" 	# TODO: Expand Student dashboard to provide all needed info
-					#@subscriptions = Subscription.where(:user_id => current_user.id).group_by(&:state)
 					@courses = current_user.courses.select('courses.*, subscriptions.state').group_by(&:state)
-					@available = Course.joins('left outer join subscriptions on subscriptions.course_id = courses.id').where('not subscriptions.user_id = ?', current_user.id).limit(5)
+					@available = Course.joins('left outer join subscriptions on subscriptions.course_id = courses.id').where('not subscriptions.user_id = ?', current_user.id).group(:id).limit(5)
 				else
 					return false
 					
