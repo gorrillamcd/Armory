71fe98323658a24ac6313639a30cc6b7c9bccbe1
Timothy Kaemmerer <gorrillamcdouglas@gmail.com>
2012-12-05 19:12:14 -0800

    updated deploy.rb to symlink stripe initializer

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/initializers/stripe.rb/ #{release_path}/config/initializers/stripe.rb/"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
